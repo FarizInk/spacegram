cc7b390a8d314adfd91bd1c55171980f2b868008
Fariz <nizaralfariziakbar10@gmail.com>
2024-02-25 10:41:31 +0700

    install phosphor icon & sonner popup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { onMount } from 'svelte';
 	import '../app.css';
+	import { Toaster } from 'svelte-sonner';
+	import { onMount } from 'svelte';
 
 	onMount(() => {
 		if (
@@ -16,6 +17,7 @@
 	});
 </script>
 
+<Toaster richColors position="bottom-center" />
 <div class="container mx-auto max-w-4xl min-h-screen flex flex-col items-center justify-center">
 	<slot />
 </div>
